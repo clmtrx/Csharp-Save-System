61099cc97074004d81fab0ae32a9f29bcea2e1d8
Cl1m <86411450+cl1m-dev@users.noreply.github.com>
2022-02-21 01:47:36 +0300

    main update

--- SaveSystem.cs
+++ SaveSystem.cs
@@ -3,48 +3,47 @@ using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
 
+namespace ClimatrixSave{
+    public static class SaveSystem
+    {
+        //Enter Your Save Path here vv
+        private static string savePath => "here!";
+        private static List<string> saveContent { get { return File.ReadAllLines(savePath).ToList<string>(); } }
 
-public static class SaveSystem
-{
-    private static string savePath => Application.persistentDataPath + "beat_crash.dat";
-    private static List<string> saveContent { get { return File.ReadAllLines(savePath).ToList<string>(); } }
-
-    public static bool downScroll => ReadData<int>("downScroll", 1) > 0;
-
-    public static void SaveData<T>(string key, T data){
-        if (!saveContent.Contains(key + ":")){
-            var newSave = saveContent;
-            newSave.Add(key+":"+data.ToString());
-
-            File.WriteAllLines(savePath, newSave);
-        }
-        else
-            for(int i = 0; i < saveContent.Count; i++)
-                if (saveContent[i].StartsWith(key + ":")){
-                    var newSave = saveContent;
-                    newSave[i] = key + ":" + data.ToString();
-                    File.WriteAllLines(savePath, newSave);
-                }    
-    }
-    
-    public static T ReadData<T>(string key, T def){
-        for(int i = 0; i < saveContent.Count; i++){
+        public static void SaveData<T>(string key, T data){
             if (!saveContent.Contains(key + ":")){
                 var newSave = saveContent;
-                newSave.Add(key+":"+def.ToString());
+                newSave.Add(key+":"+data.ToString());
 
                 File.WriteAllLines(savePath, newSave);
-                return def;
+            }
+            else
+                for(int i = 0; i < saveContent.Count; i++)
+                    if (saveContent[i].StartsWith(key + ":")){
+                        var newSave = saveContent;
+                        newSave[i] = key + ":" + data.ToString();
+                        File.WriteAllLines(savePath, newSave);
+                    }    
+        }
+        
+        public static T ReadData<T>(string key, T def){
+            for(int i = 0; i < saveContent.Count; i++){
+                if (!saveContent.Contains(key + ":")){
+                    var newSave = saveContent;
+                    newSave.Add(key+":"+def.ToString());
+
+                    File.WriteAllLines(savePath, newSave);
+                    return def;
+                }
+
+                if (saveContent[i].StartsWith(key + ":")) return (T)GetKeyContent(saveContent, key, i);
             }
 
-            if (saveContent[i].StartsWith(key + ":")) return (T)GetKeyContent(saveContent, key, i);
+            return def;
         }
 
-        return def;
-    }
-
-    private static object GetKeyContent(List <string> sav, string key, int line){
-        return sav[line].Split(key + ":")[1].Split("\n")[0];
+        private static object GetKeyContent(List <string> sav, string key, int line){
+            return sav[line].Split(key + ":")[1].Split("\n")[0];
+        }
     }
 }
-
