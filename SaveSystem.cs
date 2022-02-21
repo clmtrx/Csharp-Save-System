cb5f2e14c64c92096958528c797d482565821f81
Climatrix <86411450+cl1m-dev@users.noreply.github.com>
2022-02-21 19:49:15 +0300

    minor fix

--- SaveSystem.cs
+++ SaveSystem.cs
@@ -9,44 +9,45 @@ namespace ClimatrixSave{
         private static string savePath => "here!";
         public static List<string> saveContent { get => GetLines(); }
 
-    public static void SaveData<T>(string key, T data){
+        public static void SaveData<T>(string key, T data){
 
-        if (saveContent.FirstOrDefault(str => str.StartsWith(key + ":")) == null){
-            var newSave = saveContent;
-            newSave.Add(key+":"+data.ToString());
+            if (saveContent.FirstOrDefault(str => str.StartsWith(key + ":")) == null){
+                var newSave = saveContent;
+                newSave.Add(key+":"+data.ToString() + "\n");
 
-            File.WriteAllLines(savePath, newSave);
+                File.WriteAllLines(savePath, newSave);
+            }
+            else
+                for(int i = 0; i < saveContent.Count; i++)
+                    if (saveContent[i].StartsWith(key + ":")){
+                        var newSave = saveContent;
+                        newSave[i] = key + ":" + data.ToString() + "\n";
+                        File.WriteAllLines(savePath, newSave);
+                    }    
         }
-        else
-            for(int i = 0; i < saveContent.Count; i++)
-                if (saveContent[i].StartsWith(key + ":")){
-                    var newSave = saveContent;
-                    newSave[i] = key + ":" + data.ToString();
-                    File.WriteAllLines(savePath, newSave);
-                }    
-    }
-    
-    public static T ReadData<T>(string key, T def){
-        if (saveContent.FirstOrDefault(str => str.StartsWith(key + ":")) == null){
-            var newSave = saveContent;
-            newSave.Add(key+":"+def.ToString());
 
-            File.WriteAllLines(savePath, newSave);
+        public static T ReadData<T>(string key, T def){
+            if (saveContent.FirstOrDefault(str => str.StartsWith(key + ":")) == null){
+                var newSave = saveContent;
+                newSave.Add(key+":"+def.ToString()+"\n");
+
+                File.WriteAllLines(savePath, newSave);
+                return def;
+            }
+
+            for(int i = 0; i < saveContent.Count; i++)
+                if (saveContent[i].StartsWith(key + ":")) return GetKeyContent<T>(saveContent, key, i);
+
             return def;
         }
 
-        for(int i = 0; i < saveContent.Count; i++)
-            if (saveContent[i].StartsWith(key + ":")) return GetKeyContent<T>(saveContent, key, i);
+        private static T GetKeyContent<T>(List <string> sav, string key, int line){
+            return (T)Convert.ChangeType(sav[line].Split(key + ":")[1].Split("\n")[0], typeof(T));
+        }
 
-        return def;
-    }
-
-    private static T GetKeyContent<T>(List <string> sav, string key, int line){
-        return (T)Convert.ChangeType(sav[line].Split(key + ":")[1].Split("\n")[0], typeof(T));
-    }
-
-    private static List<string> GetLines(){
-        if (!File.Exists(savePath)) File.Create(savePath).Dispose();
-        return File.ReadAllLines(savePath).ToList<string>();
-    }
+        private static List<string> GetLines(){
+            if (!File.Exists(savePath)) File.Create(savePath).Dispose();
+            return File.ReadAllLines(savePath).ToList<string>();
+        }
+    )
 }
